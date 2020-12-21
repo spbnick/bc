c51f79d24836171b33d8b105f7d8d5a684e5aac8
Nikolai Kondrashov <spbnick@gmail.com>
2020-12-21 13:29:04 +0200

    Remove commented-out prototype code

--- bc.c
+++ bc.c
@@ -281,44 +281,4 @@ main(void)
                      TSC_VALUE_LIST[3],
                      TSC_VALUE_LIST[4]);
     }
-#if 0
-    /* Enable clock to GPIO ports A and B */
-    RCC->iopenr |= RCC_IOPENR_IOPAEN_MASK | RCC_IOPENR_IOPBEN_MASK;
-
-    /* Configure the PB8 pin for push-pull output */
-    GPIO_B->otyper = (GPIO_B->otyper & ~GPIO_OTYPER_OT8_MASK) |
-                     (GPIO_OTYPE_PUSH_PULL << GPIO_OTYPER_OT8_LSB);
-    GPIO_B->moder = (GPIO_B->moder & ~GPIO_MODER_MODE8_MASK) |
-                    (GPIO_MODE_OUTPUT << GPIO_MODER_MODE8_LSB);
-
-    /* Configure the PB9 pin for fast push-pull output */
-    GPIO_B->otyper = (GPIO_B->otyper & ~GPIO_OTYPER_OT9_MASK) |
-                     (GPIO_OTYPE_PUSH_PULL << GPIO_OTYPER_OT9_LSB);
-    GPIO_B->moder = (GPIO_B->moder & ~GPIO_MODER_MODE9_MASK) |
-                    (GPIO_MODE_OUTPUT << GPIO_MODER_MODE9_LSB);
-    GPIO_B->ospeedr = (GPIO_B->ospeedr & ~GPIO_OSPEEDR_OSPEED9_MASK) |
-                      (GPIO_OSPEED_HIGH << GPIO_OSPEEDR_OSPEED9_LSB);
-
-    /* Set PB8 high */
-    GPIO_B->odr |= GPIO_ODR_OD8_MASK;
-    /* Forever */
-    while (1) {
-        /* Wait */
-        for (i = 0; i < 286; i++);
-        /* Toggle PB9 */
-        GPIO_B->odr ^= GPIO_ODR_OD9_MASK;
-        /* Toggle PB8 */
-        GPIO_B->odr ^= GPIO_ODR_OD8_MASK;
-    }
-
-    /* Enable TSC */
-    /* Enable LED GPIO */
-    /* Enable the serial port */
-
-    /* Stat LED lighting cycle */
-    /* Start TSC acquisition */
-    /* Forever */
-        /* Wait for a new TSC reading */
-        /* Output the reading to the serial port */
-#endif
 }
